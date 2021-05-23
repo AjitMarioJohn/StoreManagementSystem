f49ef69ea0e1c1d141e0e1af9a8cbbd022250a8f
ajitmariojohn <ajitmariojohn@gmail.com>
2021-05-23 19:04:24 +0530

    logging format

--- item-mgmt/presentation/src/main/java/module-info.java
+++ item-mgmt/presentation/src/main/java/module-info.java
@@ -2,5 +2,6 @@ module itemMgmt.presentation {
     requires spring.web;
     requires spring.beans;
     requires itemMgmt.domain;
+    requires org.apache.logging.log4j;
     requires com.sms.parent.common;
 }
--- sms-parent/sms-common/src/main/java/module-info.java
+++ sms-parent/sms-common/src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.sms.parent.common {
     requires spring.web;
     requires lombok;
     requires spring.context;
+    requires com.fasterxml.jackson.databind;
 
     exports com.sms.common.annotations;
+    exports com.sms.common.utils;
+    exports com.sms.common.models;
 }
